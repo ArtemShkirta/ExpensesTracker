1e62cd1325b266deef8be81c902755a20f5d1fc9
Artem Shkirta <>
2021-09-05 11:22:04 +0300

    Database
    ### feature
    - add TransactionService
    - add make data source method

--- Domain/Domain/UseCases/TransactionUseCase.swift
+++ Domain/Domain/UseCases/TransactionUseCase.swift
@@ -5,6 +5,8 @@
 //  Created by Artem Shkirta on 02.09.2021.
 //
 
+import protocol CoreData.NSFetchedResultsControllerDelegate
+
 public protocol TransactionUseCase {
-    
+    func makeDataSource(delegate: NSFetchedResultsControllerDelegate) -> AnyDataSource<Transaction>
 }
